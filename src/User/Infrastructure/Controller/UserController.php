cb5e72f5bb424fdbbca6d14d707d9aa45a1c87e2
Diego Ocares Cordova <diegoocares02@gmail.com>
2024-08-20 17:46:05 -0500

    feat: :sparkles: implement que params with utils

--- src/User/Infrastructure/Controller/UserController.php
+++ src/User/Infrastructure/Controller/UserController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Src\User\Infrastructure\Controller;
 
 use Core\Container;
+use Utils\QueryParams;
 use Src\User\Aplication\UseCase\GetUsers;
 
 class UserController
@@ -16,6 +17,9 @@ class UserController
 
     public function index()
     {
+        $param = QueryParams::query('param');
+        $page = (int) QueryParams::query('page', '1');
+        $perPage = (int) QueryParams::query('perPage', '16');
         $getUsers = $this->container->get(GetUsers::class);
         $users = $getUsers->execute();
         echo json_encode($users);
